305992a7fe9653968ca303ccc027d256e558d44c
shuibuzhuo <2301140231@qq.com>
2023-09-16 14:35:40 +0800

    chore: 修改目录结构，完善 createReactiveObject 方法

--- code/packages/reactivity/src/reactive.ts
+++ code/packages/reactivity/src/reactive.ts
@@ -1,4 +1,4 @@
-import { mutableHandlers } from "./baseHandlers"
+import { mutableHandlers } from './baseHandlers'
 
 export const reactiveMap = new WeakMap<object, any>()
 
@@ -6,7 +6,11 @@ export function reactive(target: object) {
   return createReactiveObject(target, mutableHandlers, reactiveMap)
 }
 
-function createReactiveObject(target: object, baseHandlers: ProxyHandler<any>, proxyMap: WeakMap<object, any>) {
+function createReactiveObject(
+  target: object,
+  baseHandlers: ProxyHandler<any>,
+  proxyMap: WeakMap<object, any>
+) {
   const existingProxy = proxyMap.get(target)
 
   if (existingProxy) {
@@ -14,6 +18,8 @@ function createReactiveObject(target: object, baseHandlers: ProxyHandler<any>, p
   }
 
   const proxy = new Proxy(target, baseHandlers)
+
+  proxyMap.set(target, proxy)
 
   return proxy
 }
