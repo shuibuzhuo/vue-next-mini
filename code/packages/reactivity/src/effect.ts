21cac76d2ca1aaa57ccc5eb6e77adcda609dddee
shuibuzhuo <2301140231@qq.com>
2023-09-17 21:59:54 +0800

    feat: 实现 trigger

--- code/packages/reactivity/src/effect.ts
+++ code/packages/reactivity/src/effect.ts
@@ -1,3 +1,6 @@
+type KeyToDepMap = Map<any, ReactiveEffect>
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
 export function effect<T = any>(fn: () => T) {
   const _effect = new ReactiveEffect(fn)
 
@@ -17,10 +20,26 @@ export class ReactiveEffect<T = any> {
 }
 
 export function track(target: object, key: unknown) {
-  console.log('track: 收集依赖')
-  console.log('watch2')
+  if (!activeEffect) return
+
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+
+  depsMap.set(key, activeEffect)
+
+  console.log(targetMap)
 }
 
 export function trigger(target: object, key: unknown, newValue: unknown) {
-  console.log('trigger：触发依赖')
+  const depsMap = targetMap.get(target)
+
+  if (!depsMap) return
+
+  const effect = depsMap.get(key) as ReactiveEffect
+
+  if (!effect) return
+
+  effect.run()
 }
