4cec0c7d306378b4d7c3170bbb403ff43b180a00
shuibuzhuo <2301140231@qq.com>
2023-09-17 09:27:37 +0800

    feat: createGetter & createSetter

---
+++ code/packages/reactivity/src/effect.ts
@@ -0,0 +1,8 @@
+export function track(target: object, key: unknown) {
+  console.log('track: 收集依赖'); 
+}
+
+export function trigger(target: object, key: unknown, newValue: unknown) {
+  console.log('trigger：触发依赖');
+  
+}
