2f5a3e976dc8cca668c272f19aea3939cf3323f9
shuibuzhuo <2301140231@qq.com>
2023-09-25 14:43:58 +0800

    feat: ELEMENT + TEXT_CHILDREN

--- code/packages/shared/src/index.ts
+++ code/packages/shared/src/index.ts
@@ -4,16 +4,16 @@
 export const isArray = Array.isArray
 
 /**
- * 判断是否为一个对象
- */
-export const isObject = (val: unknown) =>
-  val !== null && typeof val === 'object'
-
-/**
  * 判断值是否改变
  */
 export const hasChanged = (value: any, oldValue: any) =>
   !Object.is(value, oldValue)
+
+/**
+ * 判断是否为一个对象
+ */
+export const isObject = (val: unknown) =>
+  val !== null && typeof val === 'object'
 
 /**
  * 判断是否为一个函数
@@ -21,7 +21,13 @@ export const hasChanged = (value: any, oldValue: any) =>
 export const isFunction = (val: unknown): val is Function =>
   typeof val === 'function'
 
+/**
+ * 判断是否为一个字符串
+ */
+export const isString = (val: unknown): val is string => typeof val === 'string'
+
 /** 合并对象属性 */
 export const extend = Object.assign
 
-export const EMPTY_OBJ:  { readonly [key: string]: any} = {}
+/** 定义一个常量，是个空对象 */
+export const EMPTY_OBJ: { readonly [key: string]: any } = {}
