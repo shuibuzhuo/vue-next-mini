16e5c5caa4845fa9afbb4f596141e74a8614268a
shuibuzhuo <2301140231@qq.com>
2023-09-29 16:27:28 +0800

    feat: 更新 ELEMENT + TEXT_CHILDREN

--- code/packages/runtime-core/src/renderer.ts
+++ code/packages/runtime-core/src/renderer.ts
@@ -1,4 +1,5 @@
 import { Comment, Fragment, Text } from '@vue/runtime-core'
+import { EMPTY_OBJ } from '@vue/shared'
 import { ShapeFlags } from 'packages/shared/src/shapeFlags'
 
 export interface RendererOptions {
@@ -15,11 +16,76 @@ export function createRenderer(options: RendererOptions) {
 function baseCreateRenderer(options: RendererOptions) {
   const { createElement: hostCreateElement, setElementText: hostSetElementText, patchProp: hostPatchProp, insert: hostInsert} = options
 
+  const patchElement = (newVNode, oldVNode) => {
+    const el = (newVNode.el = oldVNode.el)
+
+    const oldProps = oldVNode.props || EMPTY_OBJ
+    const newProps = newVNode.props || EMPTY_OBJ
+
+    patchChildren(oldVNode, newVNode, el, null)
+    patchProps(el, newVNode, oldProps, newProps)
+  }
+
+  const patchChildren = (oldVNode, newVNode, container, anchor) => {
+    const c1 = oldVNode && oldVNode.children
+    const prevShapeFlag = oldVNode ? oldVNode.shapeFlag : 0
+    const c2 = newVNode.children
+    const { shapeFlag } = newVNode
+
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        // TODO 卸载旧子节点
+      }
+
+      if (c2 !== c1) {
+        hostSetElementText(container, c2)
+      }
+    } else {
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          // TODO: diff
+        } else {
+          // TODO: 卸载
+        }
+      } else {
+        if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+          // 删除旧的文本
+          hostSetElementText(container, '')
+        }
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          // TODO 单独挂载新子节点操作
+        }
+      }
+    }
+  }
+
+  const patchProps = (el, vnode, oldProps, newProps) => {
+    if (oldProps !== newProps) {
+      for (const key in newProps) {
+        const prev = oldProps[key]
+        const next = newProps[key]
+        if (next !== prev) {
+          hostPatchProp(el, key, prev, next)
+        }
+      }
+
+      if (oldProps !== EMPTY_OBJ) {
+        for (const key in oldProps) {
+          if (!(key in newProps)) {
+            hostPatchProp(el, key, oldProps[key], null)
+          }
+        }
+      }
+    }
+  }
+
   const processElement = (oldVNode, newVNode, container, anchor) => {
     if (oldVNode == null) {
+      // 挂载操作
       mountElement(newVNode, container, anchor)
     } else {
-      // TODO 更新操作
+      // 更新操作
+      patchElement(newVNode, oldVNode)
     }
   }
 
